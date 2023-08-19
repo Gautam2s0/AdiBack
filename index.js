8eb6c2ed7bf1a9460868a7031f543b9c012577c9
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-08-19 11:10:24 +0530

    hh

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const path = require('path')
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
 
+server.use(middlewares)
 server.use(router)
 server.listen(4500, () => {
   console.log('JSON Server is running')
