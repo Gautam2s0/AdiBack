d418c00de7706d7548b0f28f00a456abbe463755
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-08-19 11:13:39 +0530

    hh

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const path = require('path')
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const Memory = require('lowdb/adapters/Memory'); // Use the Memory adapter
+const adapter = new Memory();
+const db = jsonServer.createDatabase({ users: [] }, { adapter });
+const router = jsonServer.router(db);
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
-server.listen(4500, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+
+const port = 4500;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
